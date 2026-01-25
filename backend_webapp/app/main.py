5880153a68272348c1d957816de238059f4c2324
Diego-do-na <dominguezn0709@gmail.com>
2026-01-24 23:05:47 -0600

    Hopefully last origin

--- backend_webapp/app/main.py
+++ backend_webapp/app/main.py
@@ -26,6 +26,7 @@ origins = [
     "http://localhost:5173",
     "http://localhost:8000",
     "http:localhost:3000",
+    "https://inspections-jones-initial-circles.trycloudflare.com",
     "http://127.0.0.1:5173",
 ]
 
