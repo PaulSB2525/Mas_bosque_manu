8fbd3171cc11ceb950cbbf72a155ceb3ce10c353
Diego-do-na <dominguezn0709@gmail.com>
2026-01-26 01:34:12 -0600

    Mejorando routers

--- backend_webapp/app/routers/pacientes_r.py
+++ backend_webapp/app/routers/pacientes_r.py
@@ -1,9 +1,8 @@
-from typing import Optional, List
+from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.databases.connection import get_db
-from app.schemas.pacientes import PacienteResponse
 from app.services.pacientes_services import (
     obtener_pacientes,
     obtener_paciente_por_id,
@@ -15,20 +14,34 @@ router = APIRouter(
     tags=["Pacientes"]
 )
 
-@router.get("/buscar/nombre", response_model=List[PacienteResponse])
+# -------------------------------------------------------
+# Buscar pacientes por nombre
+# -------------------------------------------------------
+@router.get("/buscar/nombre")
 def buscar_paciente(
     nombre: Optional[str] = None,
     db: Session = Depends(get_db)
 ):
     return buscar_paciente_por_nombre(db, nombre)
 
-@router.get("/{id_paciente}", response_model=PacienteResponse)
+
+# -------------------------------------------------------
+# Obtener paciente por ID
+# -------------------------------------------------------
+@router.get("/{id_paciente}")
 def obtener_paciente(id_paciente: int, db: Session = Depends(get_db)):
     paciente = obtener_paciente_por_id(db, id_paciente)
     if not paciente:
-        raise HTTPException(404, "Paciente no encontrado")
+        raise HTTPException(
+            status_code=404,
+            detail="Paciente no encontrado"
+        )
     return paciente
 
-@router.get("/", response_model=List[PacienteResponse])
+
+# -------------------------------------------------------
+# Obtener todos los pacientes
+# -------------------------------------------------------
+@router.get("/")
 def listar_pacientes(db: Session = Depends(get_db)):
     return obtener_pacientes(db)
