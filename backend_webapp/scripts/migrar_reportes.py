1fa2efdae4bf0343142be0c7f293f8b98655156c
Diego-do-na <dominguezn0709@gmail.com>
2026-01-25 23:14:34 -0600

    Pacientes

--- backend_webapp/scripts/migrar_reportes.py
+++ backend_webapp/scripts/migrar_reportes.py
@@ -96,7 +96,7 @@ def migrar():
             FR=r["signosVitales"]["fr"] or 0,
             SpO2=r["signosVitales"]["spo2"] or 0,
             GLU=r["signosVitales"]["glu"] or 0,
-            TA=r["signosVitales"]["ta"] or "0/0"   # ✅ AQUÍ
+            TA=r["signosVitales"]["ta"] or "0/0"
         )
         
         db.add(sv)
@@ -133,7 +133,8 @@ def migrar():
 
             lugar_id=lugar.id_Lugar,
             signos_id=sv.id_Signos,
-            nivel_conciencia_id=nc.id_Nivel_Conciencia
+            nivel_conciencia_id=nc.id_Nivel_Conciencia,
+            paciente_id=paciente.id_Paciente
         )
         db.add(reporte)
         db.commit()
